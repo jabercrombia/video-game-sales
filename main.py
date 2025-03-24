dafc655cfa63874c4c6a4dbe0f882fae4535f212
Justin A <32287615+jabercrombia@users.noreply.github.com>
2025-03-24 13:06:33 -0500

    video game sales api

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
 from routes import allsales
 from routes import platform
 
-app = FastAPI()
+app = FastAPI(
+    title="Video Game API",
+    description="This is the documentation for my API endpoints.",
+    version="1.0.0"
+)
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(allsales.router, prefix="/api", tags=["allsales"])
 app.include_router(platform.router, prefix="/api", tags=["platform"])
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello from FastAPI!"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    with open("static/index.html", "r") as file:
+        return file.read()
 
-@app.get("/api/data")
-def get_data():
-    return {"data": "This is data from FastAPI"}
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
